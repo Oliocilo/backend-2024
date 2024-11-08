074aff83cd3694baa787b9bd88e3f3ab7ec90fd2
Oliocilo <rijl.kholik03@gmail.com>
2024-11-08 16:28:27 +0700

    Student-api

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,33 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Student;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
       public function index() {
         // menampilkan data students dari database
         $students = Student::all();
-
+        
+        if ($students) {
         $data = [
             'message' => 'Get all students',
             'data' => $students
         ];
+    } else {
+        $data = [
+            'message' => 'Student is empty'
+        ];
+    }
 
         return response()->json($data, 200);
     }
+
     public function store(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'nama' => 'required',
+            'nim' => 'numeric|required',
+            'email' => 'email|required',
+            'jurusan' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation errors',
+                'error' => $validator->errors()
+            ], 422);
+        }
 
-        $input = [
-            'nama' => $request->nama,
-            'nim' => $request->nim,
-            'email' => $request->email,
-            'jurusan' => $request->jurusan
-        ];
-        // menampilkan data students dari database
-        $students = Student::create($input);
-
+        $students = Student::create($request->all());
         $data = [
-            'message' => 'students is created succesfuly',
+            'message' => 'Student is created successfully',
             'data' => $students
         ];
 
